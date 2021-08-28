22af780defaaef3c68b2ea7a05b596bdc77454cf
innopearl <innopearl@localhost.home>
2021-08-27 21:26:42 -0400

    add test

--- 02_unittest_vs_pytest/try_unittest.py
+++ 02_unittest_vs_pytest/try_unittest.py
@@ -7,8 +7,11 @@ def cube(n):
     return n*n*n
  
 class Test(unittest.TestCase):
-    def test_square(self):
+    def test_square_2(self):
         self.assertEqual(square(2), 4)
  
+    def test_square_3(self):
+        self.assertEqual(square(3), 9)
+
     def test_cube(self):
         self.assertEqual(cube(2), 8)
